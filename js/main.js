c3045b15b229ac9c0f772ba771f905ca10ad635e
Władysław Kulik <kulikboxx@gmail.com>
2021-08-04 21:52:50 +0200

    commit

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 let slides = document.querySelectorAll('.slide'),
     current = document.querySelector('.current'),
     total = document.querySelector('.total'),
+    indicatorsList,
     currentSlide = 0;
 
 const initSlider = () => {
@@ -10,13 +11,23 @@ const initSlider = () => {
         slide.style.width = sliderMain.offsetWidth + 'px';
         document.querySelector('.slider-wrapper').style.width = sliderMain.offsetWidth * slides.length + 'px';
     });
-
     current.textContent = `0${currentSlide + 1}`;
+
+    indicatorsList = document.createElement('ol');
+    indicatorsList.classList.add('indicator-list');
+
+    for (let i = 0; i < slides.length; i++) {
+        let indicatorItem = document.createElement('li');
+        indicatorItem.classList.add('indicator-item');
+        indicatorsList.append(indicatorItem);
+    }
+    sliderMain.append(indicatorsList);
+    document.querySelector('.indicator-item:first-child').classList.add('active-item');
 }
 
-slides.length < 10 ? total.textContent = `0${slides.length}` : total.textContent = slides.length;
-
 initSlider();
+
+slides.length < 10 ? total.textContent = `0${slides.length}` : total.textContent = slides.length;
 
 const flipSlide = () => {
     if (currentSlide === slides.length) currentSlide = 0;
@@ -24,6 +35,13 @@ const flipSlide = () => {
 
     slides.forEach(slide => slide.style.transform = `translate(-${currentSlide * slide.offsetWidth}px)`);
     currentSlide < 9 ? current.textContent = `0${currentSlide + 1}` : current.textContent = currentSlide + 1;
+
+    const liItems = document.querySelectorAll('.indicator-item');
+
+    for (let i = 0; i < liItems.length; i++) {
+        liItems[i].classList.remove('active-item');
+        liItems[currentSlide].classList.add('active-item');
+    }
 }
 
 document.addEventListener('click', e => {
@@ -46,4 +64,16 @@ document.addEventListener('keydown', e => {
     }
 });
 
+const changeIndicator = e => {
+    document.querySelectorAll('.indicator-item').forEach((item, i) => {
+        item.classList.remove('active-item');
+        if (e.target === item) {
+            currentSlide = i;
+            flipSlide();
+            e.target.classList.add('active-item');
+        }
+    });
+}
+
+indicatorsList.addEventListener('click', changeIndicator);
 window.addEventListener('resize', initSlider);
